75fd7e598805ae6e971e6120420fbd606eee83f3
Alessandra Lemos <allalessandra582@gmail.com>
2024-06-02 16:30:41 -0300

    update

--- src/main/java/br/sandy/lyricsSearch/MainExample.java
+++ src/main/java/br/sandy/lyricsSearch/MainExample.java
@@ -5,37 +5,11 @@ import br.sandy.lyricsSearch.Model.MusicRegistration;
 import org.apache.hc.core5.http.ParseException;
 import java.io.IOException;
 
-// Testando a classe MusicRegistration
+// Busca de músicas online através de API
 
 public class MainExample {
     public static void main(String[] args){
         MusicRegistration musicReg = new MusicRegistration();
-
-        // Adicionando músicas localmente
-        Music music1 = new Music("1", "Title1", "Artist1", "Album1", "Lyric1");
-        musicReg.addMusic(music1);
-
-        Music music2 = new Music("2", "Title2", "Artist2", "Album2", "Lyric2");
-        musicReg.addMusic(music2);
-
-        // Listando músicas locais
-        System.out.println("Músicas loais: ");
-        for(Music music : musicReg.listMusic()){
-            System.out.println(music.getTitle());
-        }
-
-        // Buscando uma múscia localmente
-        Music searchedMusic = musicReg.searchMusic("Title1", null, null , null , null);
-        System.out.println("Música local encontrada: " + searchedMusic.getTitle());
-
-        // Removendo uma música localmente
-        musicReg.removeMusic("Title2");
-
-        // Confirmando remoção
-        System.out.println("Músicas locais após remoção:");
-        for(Music music : musicReg.listMusic()){
-            System.out.println(music.getTitle());
-        }
 
         // Buscando músicas online
         try{
--- src/main/java/br/sandy/lyricsSearch/Model/MusicRegistration.java
+++ src/main/java/br/sandy/lyricsSearch/Model/MusicRegistration.java
@@ -1,46 +1,16 @@
 package br.sandy.lyricsSearch.Model;
 
 import br.sandy.lyricsSearch.Dao.MusicDao;
-import br.sandy.lyricsSearch.Dao.MusicDaoLocalImpl;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.ArrayList;
 
 public class MusicRegistration {
-    private List<Music> localMusics;
     private MusicDao musicDao;
 
     public MusicRegistration() {
-        localMusics = new ArrayList<>();
         musicDao = new MusicDao();
-    }
-
-    public void addMusic(Music music) {
-        localMusics.add(music);
-    }
-
-    public List<Music> listMusic() {
-        return new ArrayList<>(localMusics);
-    }
-
-    public Music searchMusic(String title, String artist, String album, String lyric, String id) {
-        for (Music music : localMusics) {
-            boolean matches = (title == null || music.getTitle().equalsIgnoreCase(title)) &&
-                    (artist == null || music.getArtist().equalsIgnoreCase(artist)) &&
-                    (album == null || music.getAlbum().equalsIgnoreCase(album)) &&
-                    (lyric == null || music.getLyric().equalsIgnoreCase(lyric)) &&
-                    (id == null || music.getId().equalsIgnoreCase(id));
-            if (matches) {
-                return music;
-            }
-        }
-        return null;
-    }
-
-    public void removeMusic(String title) {
-        localMusics.removeIf(music -> music.getTitle().equalsIgnoreCase(title));
     }
 
     public List<Music> searchMusicOnline(String query) throws IOException, ParseException {
